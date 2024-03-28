4b30566a9d3f1aa102f442ed5a901062fb3d20e0
Zayan Shamz <zaynshamz0202@gmail.com>
2024-03-29 01:22:50 +0530

    live cursor

---
+++ liveblocks.config.ts
@@ -0,0 +1,99 @@
+import { createClient } from "@liveblocks/client";
+import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
+
+const client = createClient({
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!
+});
+
+
+type Presence = {
+  // cursor: { x: number, y: number } | null,
+  // ...
+};
+
+
+type Storage = {
+  // author: LiveObject<{ firstName: string, lastName: string }>,
+  // ...
+};
+
+
+type UserMeta = {
+  // id?: string,  // Accessible through `user.id`
+  // info?: Json,  // Accessible through `user.info`
+};
+
+type RoomEvent = {
+  // type: "NOTIFICATION",
+  // ...
+};
+
+
+export type ThreadMetadata = {
+  // resolved: boolean;
+  // quote: string;
+  // time: number;
+};
+
+// Room-level hooks, use inside `RoomProvider`
+export const {
+  suspense: {
+    RoomProvider,
+    useRoom,
+    useMyPresence,
+    useUpdateMyPresence,
+    useSelf,
+    useOthers,
+    useOthersMapped,
+    useOthersListener,
+    useOthersConnectionIds,
+    useOther,
+    useBroadcastEvent,
+    useEventListener,
+    useErrorListener,
+    useStorage,
+    useObject,
+    useMap,
+    useList,
+    useBatch,
+    useHistory,
+    useUndo,
+    useRedo,
+    useCanUndo,
+    useCanRedo,
+    useMutation,
+    useStatus,
+    useLostConnectionListener,
+    useThreads,
+    useCreateThread,
+    useEditThreadMetadata,
+    useCreateComment,
+    useEditComment,
+    useDeleteComment,
+    useAddReaction,
+    useRemoveReaction,
+    useThreadSubscription,
+    useMarkThreadAsRead,
+    useRoomNotificationSettings,
+    useUpdateRoomNotificationSettings,
+
+    // These hooks can be exported from either context
+    // useUser,
+    // useRoomInfo
+  }
+} = createRoomContext<Presence, Storage, UserMeta, RoomEvent, ThreadMetadata>(client);
+
+// Project-level hooks, use inside `LiveblocksProvider`
+export const {
+  suspense: {
+    LiveblocksProvider,
+    useMarkInboxNotificationAsRead,
+    useMarkAllInboxNotificationsAsRead,
+    useInboxNotifications,
+    useUnreadInboxNotificationsCount,
+
+    // These hooks can be exported from either context
+    useUser,
+    useRoomInfo,
+  }
+} = createLiveblocksContext<UserMeta, ThreadMetadata>(client);
