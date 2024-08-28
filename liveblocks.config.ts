dfa6ff8f2acd19e32cb34a74153a24521825f69a
Zayan Shamz <zaynshamz0202@gmail.com>
2024-08-28 14:53:10 +0530

    tools and shapes menu 2

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@liveblocks/client";
+import { LiveMap, createClient } from "@liveblocks/client";
 import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
 
 const client = createClient({
@@ -15,6 +15,7 @@ type Presence = {
 type Storage = {
   // author: LiveObject<{ firstName: string, lastName: string }>,
   // ...
+  canvasObjects: LiveMap<string, any>
 };
 
 
